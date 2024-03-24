8810ee07a81fc2cc71f3559cfd425a223ed5a13f
Jakov C <qwerasdftzu@gmail.com>
2024-03-24 17:30:57 +0100

    works

--- backend/server.js
+++ backend/server.js
@@ -425,6 +425,57 @@ Result: "${result}"
   });
 });
 
+app.get("/users/all", verifyToken, async (req, res) => {
+  try {
+    // Ensure only admins can access this route
+    if (req.userRole !== "admin") {
+      return res
+        .status(401)
+        .json({ message: "Only admins can get users info." });
+    }
+
+    // Fetch all users from the database
+    const users = await User.find({}, "username role rounds");
+
+    // Prepare the response array
+    const usersInfo = users.map((user) => ({
+      id: user._id,
+      username: user.username,
+      role: user.role,
+      rounds: user.rounds,
+    }));
+
+    // Send the response array
+    res.status(200).json(usersInfo);
+  } catch (error) {
+    // Handle errors
+    res.status(500).json({ message: error.message });
+  }
+});
+
+app.delete("/users/:userId", verifyToken, async (req, res) => {
+  try {
+    // Ensure only admins can access this route
+    if (req.userRole !== "admin") {
+      return res.status(401).json({ message: "Only admins can delete users." });
+    }
+
+    const userId = req.params.userId; // Id of user to delete
+
+    // Delete user with this id.
+    const deletedUser = await User.findByIdAndDelete(userId);
+
+    if (!deletedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    return res.status(200).json({ message: "User deleted successfully." });
+  } catch (error) {
+    // Handle errors
+    return res.status(500).json({ message: error.message });
+  }
+});
+
 // Start the server on the specified port
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
