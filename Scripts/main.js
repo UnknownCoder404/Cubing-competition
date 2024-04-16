b089c946fa1df1b9eee65b42c4f25527e81f903c
Jakov <qwerasdftzu@gmail.com>
2024-04-16 16:45:03 +0200

    works

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,5 +1,17 @@
 const logInEle = document.querySelector(".js-log-in");
 const username = localStorage.getItem("username");
+const cardsDiv = document.querySelector(".cards");
 if (username) {
   logInEle.innerHTML = username;
+}
+const role = localStorage.getItem("role");
+
+if (role === "admin") {
+  let html = "";
+  html += `
+  <div class="card">
+      <h2>Radna ploča</h2>
+      <p>Ti si admin. Oni imaju pristup <a href="./dashboard/dashboard.html">radnoj ploči!</a></p>
+  `;
+  cardsDiv.insertAdjacentHTML("beforeEnd", html);
 }
