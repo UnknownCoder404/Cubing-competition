589849ea51454fe4bf6ebab4268c5fce0c02a3ad
Jakov <qwerasdftzu@gmail.com>
2024-04-20 12:31:23 +0200

    Loading spinner at login

--- build/Login/login.js
+++ build/Login/login.js
@@ -1,11 +1,21 @@
 const url = "https://cubing-competition.onrender.com";
+const loadingHTML = `<div id="circularG">
+<div id="circularG_1" class="circularG"></div>
+<div id="circularG_2" class="circularG"></div>
+<div id="circularG_3" class="circularG"></div>
+<div id="circularG_4" class="circularG"></div>
+<div id="circularG_5" class="circularG"></div>
+<div id="circularG_6" class="circularG"></div>
+<div id="circularG_7" class="circularG"></div>
+<div id="circularG_8" class="circularG"></div>
+</div>`;
 document
   .getElementById("loginForm")
   .addEventListener("submit", async function (event) {
     event.preventDefault();
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
-
+    document.querySelector(".submit-btn").innerHTML = loadingHTML;
     try {
       const response = await fetch(`${url}/login`, {
         method: "POST",
@@ -17,6 +27,7 @@ document
 
       const data = await response.json();
       document.getElementById("message").innerText = data.message;
+      document.querySelector(".submit-btn").innerHTML = "Prijava";
       if (response.ok) {
         // Save token to local storage or session storage
         localStorage.setItem("token", data.info.token);
