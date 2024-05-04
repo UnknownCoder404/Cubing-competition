aee78064fb432c07cbc7cecf48a2ba9703494d15
Jakov <qwerasdftzu@gmail.com>
2024-05-04 15:46:16 +0200

    Seperate login route

---
+++ backend/routes/login.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../Models/user");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+// Define a route for user login
+router.post("/", async (req, res) => {
+  try {
+    // Get the username and password from the request body
+    const username = req.body.username;
+    const password = req.body.password;
+    // Validate the input
+    if (!username || !password) {
+      return res
+        .status(400)
+        .json({ message: "Korisničko ime i lozinka su obavezni." });
+    }
+    // Find the user by username
+    const user = await User.findOne({ username });
+    // Check if the user exists
+    if (!user) {
+      return res.status(404).json({ message: "Korisnik ne postoji." });
+    }
+    // Compare the password with the hashed one
+    const match = await user.comparePassword(password);
+    // Check if the password matches
+    if (!match) {
+      return res.status(401).json({ message: "Netočna lozinka." });
+    }
+    // Generate a JSON web token with the user id as the payload
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    res.status(200).json({
+      message: "Korisnik se uspješno prijavio.",
+      info: { id: user._id, token, username: username, role: user.role },
+    });
+  } catch (err) {
+    // Handle the error
+    res.status(500).json({ message: err.message });
+  }
+});
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -50,44 +50,7 @@ mongoose
 
 console.log(getCurrentDateTimeInZagreb());
 app.use("/register", require("./routes/register"));
-// Define a route for user login
-app.post("/login", async (req, res) => {
-  try {
-    // Get the username and password from the request body
-    const username = req.body.username;
-    const password = req.body.password;
-    // Validate the input
-    if (!username || !password) {
-      return res
-        .status(400)
-        .json({ message: "Korisničko ime i lozinka su obavezni." });
-    }
-    // Find the user by username
-    const user = await User.findOne({ username });
-    // Check if the user exists
-    if (!user) {
-      return res.status(404).json({ message: "Korisnik ne postoji." });
-    }
-    // Compare the password with the hashed one
-    const match = await user.comparePassword(password);
-    // Check if the password matches
-    if (!match) {
-      return res.status(401).json({ message: "Netočna lozinka." });
-    }
-    // Generate a JSON web token with the user id as the payload
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
-    res.status(200).json({
-      message: "Korisnik se uspješno prijavio.",
-      info: { id: user._id, token, username: username, role: user.role },
-    });
-  } catch (err) {
-    // Handle the error
-    res.status(500).json({ message: err.message });
-  }
-});
-
+app.use("/login", require("./routes/login"));
 app.get("/profile", verifyToken, async (req, res) => {
   try {
     const user = await User.findbyId(req.userId);
