831bbf425b734ee0b38b59d2b46d3fa9f53c0ec1
Jakov <qwerasdftzu@gmail.com>
2024-05-21 11:44:31 +0200

    Allow 15 login attempts

--- backend/rateLimiter/login.js
+++ backend/rateLimiter/login.js
@@ -2,7 +2,7 @@ const rateLimit = require("express-rate-limit");
 // Specific rate limit for the login route
 const loginLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 15, // limit each IP to 5 login requests per windowMs
+  max: 15, // limit each IP to 15 login requests per windowMs
   message: "Too many login attempts, please try again after 15 minutes",
 });
 module.exports = loginLimiter;
