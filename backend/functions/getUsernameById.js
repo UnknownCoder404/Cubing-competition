7976a371e456c0cf5ffdcd55934d2e65ede1196b
Jakov <qwerasdftzu@gmail.com>
2024-05-23 18:57:05 +0200

    Fix a bug at getting winners

--- backend/functions/getUsernameById.js
+++ backend/functions/getUsernameById.js
@@ -1,7 +1,7 @@
 const User = require("../Models/user");
 async function getUsernameById(id) {
   try {
-    const user = await User.findById(winners[index].id);
+    const user = await User.findById(id).select("+username");
     return user.username ? user.username : null;
   } catch (err) {
     console.error(err);
