5e81422500b3e6f6a4d4024bcf382895b50b4179
Jakov <qwerasdftzu@gmail.com>
2024-05-23 22:26:36 +0200

    [BUGFIX]: Fix a bug at getting users route

--- backend/functions/getUserById.js
+++ backend/functions/getUserById.js
@@ -1,11 +1,12 @@
 const User = require("../Models/user");
-async function getUserById(id) {
+
+async function getUserById(id, fields = null) {
   try {
-    const user = await User.findOne({
-      _id: {
-        $eq: id,
-      },
-    });
+    let query = User.findOne({ _id: id });
+    if (fields) {
+      query = query.select(fields);
+    }
+    const user = await query;
     return user ? user : null;
   } catch (err) {
     console.error(err);
