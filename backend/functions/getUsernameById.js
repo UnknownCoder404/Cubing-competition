40011a4822cedfad21b504b0ec7564fe769473d0
Jakov <qwerasdftzu@gmail.com>
2024-05-25 12:38:40 +0200

    Make function simpler

--- backend/functions/getUsernameById.js
+++ backend/functions/getUsernameById.js
@@ -1,12 +1,8 @@
 const User = require("../Models/user");
 async function getUsernameById(id) {
   try {
-    const user = await User.findOne({
-      _id: {
-        $eq: id,
-      },
-    }).select("+username");
-    return user.username ? user.username : null;
+    const user = await User.findById(id);
+    return user ? (user.username ? user.username : null) : null;
   } catch (err) {
     console.error(err);
     return null;
