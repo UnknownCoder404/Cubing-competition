4bd3832892714631da92360e8a03441a1562d24d
Jakov <qwerasdftzu@gmail.com>
2024-05-30 19:24:26 +0200

    Import loading html instead

--- build/advanced-dashboard/a-dashboard.js
+++ build/advanced-dashboard/a-dashboard.js
@@ -1,16 +1,5 @@
 import { getToken, tokenValid } from "../Scripts/credentials.js";
-import { url } from "../Scripts/variables.js";
-const loadingHTML = `<div id="circularG">
-<div id="circularG_1" class="circularG"></div>
-<div id="circularG_2" class="circularG"></div>
-<div id="circularG_3" class="circularG"></div>
-<div id="circularG_4" class="circularG"></div>
-<div id="circularG_5" class="circularG"></div>
-<div id="circularG_6" class="circularG"></div>
-<div id="circularG_7" class="circularG"></div>
-<div id="circularG_8" class="circularG"></div>
-</div>`;
-
+import { url, loadingHTML } from "../Scripts/variables.js";
 String.prototype.addToken = function (token = getToken()) {
   return `${this}${this.includes("?") ? "&" : "?"}token=${token}`;
 };
