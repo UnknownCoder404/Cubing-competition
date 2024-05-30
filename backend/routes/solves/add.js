c96b5863f3a5e8ef716001ffa2ed04f8022c9f40
Jakov <qwerasdftzu@gmail.com>
2024-05-30 21:54:12 +0200

    Add trycatch to handle errors

--- backend/routes/solves/add.js
+++ backend/routes/solves/add.js
@@ -5,44 +5,50 @@ const addSolves = require("../../functions/addSolves");
 const { getUserById } = require("../../functions/getUserById");
 const router = express.Router();
 router.post("/:solverId", verifyToken, async (req, res) => {
-  const solverId = req.params.solverId;
-  const solver = await getUserById(solverId);
-  const judgeId = req.userId;
-  const judgeRole = req.userRole;
-  const solves = req.body.solves;
-  const round = req.body.round - 1; // indexing starts at 0
-  if (judgeRole !== "admin") {
-    return res
-      .status(403)
-      .json({ message: "Samo administratori mogu dodavati slaganja." });
-  }
-  if (!solver) {
-    return res.status(400).json({
-      message: `Natjecatelj ne postoji. Kontaktirajte programere za pomoć. (Naveli ste: ${solverId})`,
-    });
-  }
-  if (!solves) {
-    return res.status(400).json({ message: "Nema ponuđenih slaganja." });
-  }
-  for (let i = 0; i < solves.length; i++) {
-    if (solves !== 0 && !solves[i]) {
-      return res.status(400).json({ message: `Slaganje ne postoji.` });
-    }
-    if (solves[i] < 0) {
+  try {
+    const solverId = req.params.solverId;
+    const solver = await getUserById(solverId);
+    const judgeId = req.userId;
+    const judgeRole = req.userRole;
+    const solves = req.body.solves;
+    const round = req.body.round - 1; // indexing starts at 0
+    if (judgeRole !== "admin") {
       return res
-        .status(400)
-        .json({ message: `Slaganje #${i + 1} je negativno.` });
+        .status(403)
+        .json({ message: "Samo administratori mogu dodavati slaganja." });
     }
+    if (!solver) {
+      return res.status(400).json({
+        message: `Natjecatelj ne postoji. Kontaktirajte programere za pomoć. (Naveli ste: ${solverId})`,
+      });
+    }
+    if (!solves) {
+      return res.status(400).json({ message: "Nema ponuđenih slaganja." });
+    }
+    for (let i = 0; i < solves.length; i++) {
+      if (solves !== 0 && !solves[i]) {
+        return res.status(400).json({ message: `Slaganje ne postoji.` });
+      }
+      if (solves[i] < 0) {
+        return res
+          .status(400)
+          .json({ message: `Slaganje #${i + 1} je negativno.` });
+      }
+    }
+    const response = await addSolves(solver, solves, round);
+    if (response > 0) {
+      return res
+        .status(200)
+        .json({ message: `Slaganje dodano korisniku ${solver.username}.` });
+    } else {
+      return res.status(400).json({
+        message: `Nije uspjelo dodavanje slaganja korisniku ${solver.username}. Kontaktirajte programere za pomoć.`,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Neuspjelo dodavanje slaganja. Greška u serveru." });
   }
-  const response = await addSolves(solver, solves, round);
-  if (response > 0) {
-    return res
-      .status(200)
-      .json({ message: `Slaganje dodano korisniku ${solver.username}.` });
-  } else {
-    return res.status(400).json({
-      message: `Nije uspjelo dodavanje slaganja korisniku ${solver.username}. Kontaktirajte programere za pomoć.`,
-    });
-  }
+ 
 });
 module.exports = router;
