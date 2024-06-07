7ff46435194eef48abb61de3c94e00975802d77c
Jakov <qwerasdftzu@gmail.com>
2024-06-07 21:40:14 +0200

    Use helpers for cleaner code

--- backend/routes/posts/new.js
+++ backend/routes/posts/new.js
@@ -2,15 +2,11 @@ const express = require("express");
 const Post = require("../../Models/post");
 const User = require("../../Models/user");
 const verifyToken = require("../../middleware/verifyToken");
+const isAdmin = require("../../utils/helpers/isAdmin");
+const findUser = require("../../utils/helpers/findUser");
 const router = express.Router();
-router.post("/", verifyToken, async (req, res) => {
-  if (req.userRole !== "admin") {
-    return res
-      .status(400)
-      .json({ message: "Samo administratori smiju objavljivati." });
-  }
-  const userId = req.userId;
-  const user = User.findById(userId);
+router.post("/", verifyToken, isAdmin, findUser, async (req, res) => {
+  const user = req.user;
   const username = user.username;
   const { title, description } = req.body;
 
