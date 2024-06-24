1736f1fe79f1a88dd5e5852c5140c712d6366be1
Jakov <qwerasdftzu@gmail.com>
2024-06-24 22:21:44 +0200

    Minor improvements

--- backend/utils/helpers/isAdmin.js
+++ backend/utils/helpers/isAdmin.js
@@ -1,7 +1,7 @@
 const isAdmin = (req, res, next) => {
   if (req.userRole !== "admin") {
     return res
-      .status(400)
+      .status(401)
       .json({ message: "Samo administratori imaju pristup." });
   }
   next();
